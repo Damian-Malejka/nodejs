f56b3b62ab24e657c03a0d73a02c4b22a15de6e1
Damian <damian.malejka@gmail.com>
2024-02-21 20:07:32 +0100

    a

--- komponenty/wstepkomponenty/src/app/app.component.ts
+++ komponenty/wstepkomponenty/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Rodzaj, Samochod} from './interfejs_samochod';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,52 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'wstepkomponenty';
+  samochody: Samochod[]=
+  [
+    {
+      marka:"McLaren",
+      model: "720S",
+      zdjecie: "assets/images/mclaren.jpg",
+      dane:[
+        "do setki 3,5 s",
+        "cena 0,5 mln euro"
+      ],
+    type: Rodzaj.sportowy
+    },
+    {
+      marka:"Hummer",
+      model: "H5",
+      zdjecie: "assets/images/hummer.jpg",
+      dane:[
+        "do setki 18 s",
+        "cena 250 000 euro"
+      ],
+    type: Rodzaj.terenowy
+    },
+    {
+      marka:"Mercedes",
+      model: "Vario Mobil Signature",
+      zdjecie: "assets/images/vario.jpg",
+      dane:[
+        "do setki 32 s",
+        "cena 0,8 mln euro"
+      ],
+    type: Rodzaj.kamper
+    }
+  ]
+  samochod: Samochod =this.samochody[0];
+  kolorb:string="white";
+  kolort:string="black";
+  aktywna:boolean = true;
+  pokazZdjecie:boolean=false;
+
+  zmienKolor()
+  {
+    this.kolorb=this.kolorb==="white"?"black":"white";
+    this.kolort=this.kolort==="black"?"white":"black";
+  }
+  Rodzaj=Rodzaj;
+  constructor(){
+    
+  }
 }
